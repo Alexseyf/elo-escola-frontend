b493284b908fb2011e4b775784e0635b699444cf
Alexandre Seyffert <alexseyf66@gmail.com>
2026-01-28 19:48:46 -0300

    fix: allow empty string in API_URL

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"
+    API_URL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000"
 };
 
 export default config;
